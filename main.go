e5cc1d851cc758f2524b11ac94208916e6eed0ce
a-poor <45295232+a-poor@users.noreply.github.com>
2022-03-30 11:07:30 -0700

    Cleaning out dev branch

--- main.go
+++ main.go
@@ -1,35 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/joho/godotenv"
-	"github.com/spf13/viper"
-)
-
-var BucketName string
-
-func init() {
-	if err := godotenv.Load(); err != nil {
-		panic(err)
-	}
-
-	BucketName = os.Getenv("BUCKET_NAME")
-}
-
 func main() {
-	// Set up the CLI config data
-	viper.SetConfigName(".ls3.yaml")
-	viper.AddConfigPath("$HOME")
-
-	viper.SetDefault("Message", "Hello, World!")
-
-	// Create the app
-	app := newApp()
-
-	if err := app.Run(os.Args); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 }
