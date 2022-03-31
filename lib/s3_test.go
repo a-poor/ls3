2684378e030300067ef78911e255eb86f55740a9
Austin Poor <45295232+a-poor@users.noreply.github.com>
2022-03-31 16:43:28 -0700

    Adding placeholder S3 tests

--- lib/s3_test.go
+++ lib/s3_test.go
@@ -5,17 +5,53 @@ import (
 )
 
 func TestS3FSListContents(t *testing.T) {
+	t.Run("successful-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
 
+	})
+	t.Run("error-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
+
+	})
 }
 
 func TestS3FSChangeDir(t *testing.T) {
+	t.Run("successful-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
 
+	})
+	t.Run("error-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
+
+	})
 }
 
 func TestS3FSGetFile(t *testing.T) {
+	t.Run("successful-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
 
+	})
+	t.Run("error-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
+
+	})
 }
 
 func TestS3FSWriteFile(t *testing.T) {
+	t.Run("successful-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
 
+	})
+	t.Run("error-response", func(t *testing.T) {
+		// TODO - Add tests for success responses...
+		t.Error("test not implemented")
+
+	})
 }
